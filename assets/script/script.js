d2d5b2940689598c182c4570432a2bdc7b88a9e1
Milorad Đuković <donmilorad87@gmail.com>
2020-03-25 02:34:00 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,6 @@
 'use strict';
 
 import SlotWeels from '/slotm/assets/script/classes/slot_weels.js'; 
-import DrawLines from '/slotm/assets/script/classes/draw_lines.js'; 
-import LogoSprite from '/slotm/assets/script/classes/logo_sprite.js'; 
-
-
-const drawLines = {};
-drawLines.instance = new DrawLines();
 
 const slotWeels = {};
-slotWeels.instance = new SlotWeels(drawLines.instance);
-
-const sprite = {};
-sprite.instance = new LogoSprite()
-
+slotWeels.instance = new SlotWeels();
